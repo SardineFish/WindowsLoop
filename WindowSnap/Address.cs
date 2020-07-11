279699d0ddd34f13875ecb758d1b5d98494f1ab5
Dwscdv3 <dwscdv3@hotmail.com>
2020-07-12 02:06:16 +0800

    memory allocation overhaul

--- WindowSnap/Address.cs
+++ WindowSnap/Address.cs
@@ -2,12 +2,17 @@
 {
     public class Address
     {
-        public const int PID = 0;
-        public const int ScreenSnapRectPos = 4;
-        public const int ScreenSnapRectX = 4;
-        public const int ScreenSnapRectY = 8;
-        public const int AttachedWindowPID = 12;
-        public const int AttachmentChanged = 16;
+        public const int PID = 0x00;
+        public const int ScreenSnapRectPos = 0x04;
+        public const int ScreenSnapRectX = 0x04;
+        public const int ScreenSnapRectY = 0x08;
+        public const int AttachmentChanged = 0x10;
+        public const int DetachmentChanged = 0x11;
+        public const int MessageParam1 = 0x20;
+        public const int MessageParam2 = 0x24;
+        public const int MessageParam3 = 0x28;
+        public const int MessageParam4 = 0x2c;
+        public const int AttachedWindowPIDs = 0x40;
 
 
         public const int ViewRectMin = 0x14;
