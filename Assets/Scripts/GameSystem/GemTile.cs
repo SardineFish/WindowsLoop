b988b5e2a33b5cb0220796baa814a66d2b49a7d7
SardineFish <Sardine@live.cn>
2020-07-12 15:47:49 +0800

    Add gem

---
+++ Assets/Scripts/GameSystem/GemTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "GemTile", menuName = "Tiles/GemTile")]
+public class GemTile : UnityEngine.Tilemaps.Tile
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
